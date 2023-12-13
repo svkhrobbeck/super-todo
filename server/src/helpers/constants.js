84a7a073199c988ff01f159b215d78a9fe4e1092
svkhrobbeck <svkhrobbeck@gmail.com>
2023-12-13 23:49:58 +0500

    add urls and constants

--- server/src/helpers/constants.js
+++ server/src/helpers/constants.js
@@ -1,1 +1,6 @@
 export const modelOptions = { timestamps: true, versionKey: false };
+
+export const clientId = process.env.GOOGLE_CLIENT_ID;
+export const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
+
+
---
+++ server/src/helpers/urls.js
@@ -0,0 +1,8 @@
+export const oAuthRedirectUri = "http://localhost:3000/api/v1/auth/google";
+export const authRedirectFrontUri = "http://localhost:5173/sign-in";
+export const userinfoTokenUrl = "https://www.googleapis.com/oauth2/v3/userinfo";
+
+export const oAuthScopeUrls = [
+  "https://www.googleapis.com/auth/userinfo.email",
+  "https://www.googleapis.com/auth/userinfo.profile",
+];
