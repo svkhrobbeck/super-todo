7a585a1707ce6f982fc4d3902a5f4f17dd96a371
svkhrobbeck <svkhrobbeck@gmail.com>
2023-12-14 08:13:01 +0500

    add example env and url.js changes

--- server/src/helpers/urls.js
+++ server/src/helpers/urls.js
@@ -1,4 +1,4 @@
-export const oAuthRedirectUri = "http://localhost:3000/api/v1/auth/google";
+export const oAuthRedirectUri = process.env.OAUTH_REDIRECT_URI;
 export const frontUri = process.env.FRONT_END_BASE_URL;
 export const userinfoTokenUrl = "https://www.googleapis.com/oauth2/v3/userinfo";
 
