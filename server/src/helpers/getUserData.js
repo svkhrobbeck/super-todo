3a6663c3c12da477223dd3256118d069d79a5062
svkhrobbeck <svkhrobbeck@gmail.com>
2023-12-14 07:58:17 +0500

    getUserData helper

--- server/src/helpers/getUserData.js
+++ server/src/helpers/getUserData.js
@@ -3,7 +3,10 @@ import { userinfoTokenUrl as url } from "./urls.js";
 
 const getUserData = async access_token => {
   const { data } = await axios.get(url, { params: { access_token } });
-  return data;
+  const { name, picture, email, sub } = data;
+
+  const userData = { name, picture, email, sub };
+  return userData;
 };
 
 export default getUserData;
