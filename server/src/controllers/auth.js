187e1d2faf2d0711885133fab5ac70d91cca4f69
svkhrobbeck <svkhrobbeck@gmail.com>
2023-12-14 08:00:08 +0500

    create authGetUrlRequest

--- server/src/controllers/auth.js
+++ server/src/controllers/auth.js
@@ -4,3 +4,8 @@ import { authorizeUrl, oAuth2Client } from "../helpers/oAuth.js";
 import { cookieOptions } from "../helpers/constants.js";
 import { generateJwt } from "../helpers/token.js";
 import User from "../models/user.js";
+
+export const authGetUrlRequest = (req, res) => {
+  res.status(200).json({ url: authorizeUrl });
+};
+
