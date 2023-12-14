f5fbfa3f1748bc08fd58aadccc1a8debdc22f522
svkhrobbeck <svkhrobbeck@gmail.com>
2023-12-14 13:17:16 +0500

    complete sign-in and sign-up pages desktop version

--- client/src/pages/sign-up.jsx
+++ client/src/pages/sign-up.jsx
@@ -3,14 +3,15 @@ import { FormInput } from "../components";
 import signInWithGoogle from "../helpers/signInWithGoogle";
 import signInBtn from "/images/google_signin_btn.png";
 
-const SignInPage = () => {
+const SignUpPage = () => {
   return (
     <div className="flex flex-col justify-center items-center w-full h-screen">
       <form className="max-w-[340px] w-full flex flex-col">
-        <h2 className="font-semibold text-[38px] mb-3 text-center">Sign In</h2>
+        <h2 className="font-semibold text-[38px] mb-3 text-center">Sign Up</h2>
+        <FormInput name="name" />
         <FormInput type="email" name="email" />
         <FormInput type="password" name="Password" />
-        <button className="btn-blue">Sign In</button>
+        <button className="btn-blue">Sign Up</button>
 
         <div className="flex w-full items-center my-[10px]">
           <span className="block flex-grow-[1] h-[1px] bg-black"></span>
@@ -24,12 +25,9 @@ const SignInPage = () => {
       </button>
 
       <p>
-        don't have an account?{" "}
-        <Link className="text-blue-500" to="/sign-up">
-          Sign Up
-        </Link>
+        do you have an account? <Link className="text-blue-500" to="/sign-in">Sign In</Link>
       </p>
     </div>
   );
 };
-export default SignInPage;
+export default SignUpPage;
