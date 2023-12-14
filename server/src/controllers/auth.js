967e38ba023878a2f74907171b0341443ea98c51
svkhrobbeck <svkhrobbeck@gmail.com>
2023-12-14 08:00:19 +0500

    create authSignWithGoogle

--- server/src/controllers/auth.js
+++ server/src/controllers/auth.js
@@ -9,3 +9,25 @@ export const authGetUrlRequest = (req, res) => {
   res.status(200).json({ url: authorizeUrl });
 };
 
+export const authSignWithGoogle = async (req, res) => {
+  const code = req.query.code;
+  let user = {};
+
+  try {
+    const response = await oAuth2Client.getToken(code);
+    await oAuth2Client.setCredentials(response.tokens);
+
+    const userData = await getUserData(oAuth2Client.credentials.access_token);
+    const isUserExist = await User.findOne({ email: userData.email }).lean();
+
+    if (isUserExist) user = isUserExist;
+    else user = await User.create(userData);
+
+    const access_token = generateJwt({ userId: user._id, sub: user.sub });
+    res.cookie("access_token", access_token, cookieOptions);
+
+    res.status(200).redirect(frontUri);
+  } catch (err) {
+    console.log("google sign-in error", err);
+  }
+};
