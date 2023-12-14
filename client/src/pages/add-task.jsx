9db71655936b15c3ba051ea37ecfccd33335e2f3
svkhrobbeck <svkhrobbeck@gmail.com>
2023-12-14 13:30:52 +0500

    create add-task page

---
+++ client/src/pages/add-task.jsx
@@ -0,0 +1,12 @@
+import { Helmet } from "react-helmet";
+
+const AddTaskPage = () => {
+  return (
+    <>
+      <Helmet>
+        <title>Todo Application | Add Task</title>
+      </Helmet>
+    </>
+  );
+};
+export default AddTaskPage;
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,3 +1,5 @@
 export { default as ErrorPage } from "./error";
 export { default as SignInPage } from "./sign-in";
+export { default as SignUpPage } from "./sign-up";
 export { default as LandingPage } from "./landing";
+export { default as AddTaskPage } from "./add-task";
