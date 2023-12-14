e311f9754f5308d4d0539170f7dde5f736ca7a43
svkhrobbeck <svkhrobbeck@gmail.com>
2023-12-14 22:11:11 +0500

    format

--- server/src/controllers/user.js
+++ server/src/controllers/user.js
@@ -6,5 +6,5 @@ export const getUserProfile = async (req, res) => {
   const user = await User.findById(req.user.userId).lean();
   const filteredUser = filterObjectFields(user, "password");
 
-  res.status(StatusCodes.OK).json({ user:filteredUser });
+  res.status(StatusCodes.OK).json({ user: filteredUser });
 };
