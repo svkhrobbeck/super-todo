60f02ad3f4d729906e6a7f5cb7ec27a534a2beef
svkhrobbeck <svkhrobbeck@gmail.com>
2023-12-14 07:57:26 +0500

    add new variables

--- server/src/helpers/constants.js
+++ server/src/helpers/constants.js
@@ -1,6 +1,8 @@
-export const modelOptions = { timestamps: true, versionKey: false };
-
+export const cookieDays = new Date(Date.now() + 1000 * 60 * 60 * 24 * 7);
 export const clientId = process.env.GOOGLE_CLIENT_ID;
 export const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
+export const jwtSecret = process.env.JWT_SECRET;
+export const jwtExpiresIn = process.env.JWT_EXPIRES_IN;
 
-
+export const cookieOptions = { secure: true, expires: cookieDays };
+export const modelOptions = { timestamps: true, versionKey: false };
