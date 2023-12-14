469d140314a69d6ec5ba4f36c6abe5e39b86df0a
svkhrobbeck <svkhrobbeck@gmail.com>
2023-12-14 07:59:01 +0500

    update user model

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -1,11 +1,15 @@
 import { Schema, model } from "mongoose";
+import { modelOptions } from "../helpers/constants.js";
 
-const UserSchema = new Schema({
-  name: { type: String, default: "User" },
-  picture: { type: String, default: null },
-  password: { type: String, default: null },
-  email: String,
-  sub: String,
-});
+const UserSchema = new Schema(
+  {
+    name: { type: String, default: "User" },
+    picture: { type: String, default: null },
+    password: { type: String, default: null },
+    email: { type: String, unique: true },
+    sub: String,
+  },
+  modelOptions
+);
 
 export default model("User", UserSchema);
