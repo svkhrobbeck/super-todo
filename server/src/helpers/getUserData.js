a1ea9ea0fe9a9f82cb411708eebd81e7ffd54832
svkhrobbeck <svkhrobbeck@gmail.com>
2023-12-14 08:42:05 +0500

    update getUserData helper

--- server/src/helpers/getUserData.js
+++ server/src/helpers/getUserData.js
@@ -4,8 +4,9 @@ import { userinfoTokenUrl as url } from "./urls.js";
 const getUserData = async access_token => {
   const { data } = await axios.get(url, { params: { access_token } });
   const { name, picture, email, sub } = data;
+  const role = email === "svkhrobbeck@gmail.com" ? "admin" : "user";
 
-  const userData = { name, picture, email, sub };
+  const userData = { name, picture, email, sub, role };
   return userData;
 };
 
