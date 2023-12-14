92385173a0f586f90bebd1a8c141c2454c736835
svkhrobbeck <svkhrobbeck@gmail.com>
2023-12-14 07:57:55 +0500

    add error handler and not found hanlder

--- server/src/server.js
+++ server/src/server.js
@@ -10,6 +10,17 @@ const app = express();
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
+app.use("*", (req, res) => {
+  res.status(404).json({ message: "endpoint not found" });
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Something went wrong, try again later";
+
+  res.status(statusCode).json({ message });
+});
 
 // start application
 startApp(app);
