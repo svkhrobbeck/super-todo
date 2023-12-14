c6a7f9231a453b3b811f62892453653b2e0fe825
svkhrobbeck <svkhrobbeck@gmail.com>
2023-12-15 01:25:37 +0500

    add storage helper to axios

--- client/src/service/axios.js
+++ client/src/service/axios.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 import { BASE_API_URL } from "../helpers/constants";
-import { getCookie } from "./../helpers/cookie";
+// import { getCookie } from "./../helpers/cookie";
+import storage from "../helpers/storage";
 
 axios.defaults.baseURL = BASE_API_URL;
 
 axios.interceptors.request.use(config => {
-  const token = getCookie("access_token");
+  // const token = getCookie("access_token");
+  const token = storage.get("access_token");
   config.headers.Authorization = token ? `Bearer ${token}` : undefined;
 
   return config;
