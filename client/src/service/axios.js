c58164b2afd7edfe669a99be0c2eede316a80336
svkhrobbeck <svkhrobbeck@gmail.com>
2023-12-14 09:16:43 +0500

    set axios interceptors

--- client/src/service/axios.js
+++ client/src/service/axios.js
@@ -1,4 +1,12 @@
 import axios from "axios";
 import { BASE_API_URL } from "../helpers/constants";
+import getCookie from "./../helpers/getCookie";
 
 axios.defaults.baseURL = BASE_API_URL;
+
+axios.interceptors.request.use(config => {
+  const token = getCookie("access_token");
+  config.headers.Authorization = `Bearer ${token}`;
+
+  return config;
+});
