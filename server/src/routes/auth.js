206092564bd2deef4638abf4908040d0b813e6f1
svkhrobbeck <svkhrobbeck@gmail.com>
2023-12-14 22:14:34 +0500

    add validation to login and register

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { authGetUrlRequest, authSignWithGoogle } from "../controllers/oauth.js";
 import { loginUser, registerUser } from "../controllers/auth.js";
+import { valLogin, valRegister } from "../middlewares/validation.js";
 
 const router = Router();
 
@@ -9,7 +10,7 @@ router.post("/request", authGetUrlRequest);
 router.get("/google", authSignWithGoogle);
 
 // auth
-router.post("/register", registerUser);
-router.post("/login", loginUser);
+router.post("/register", valRegister, registerUser);
+router.post("/login", valLogin, loginUser);
 
 export default router;
