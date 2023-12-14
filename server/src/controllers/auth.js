34a8318701ee90949f7a62c91eeba5aabc3725fa
svkhrobbeck <svkhrobbeck@gmail.com>
2023-12-14 22:11:33 +0500

    complete login and register controllers

--- server/src/controllers/auth.js
+++ server/src/controllers/auth.js
@@ -1,17 +1,30 @@
 import { StatusCodes } from "http-status-codes";
 import User from "../models/user.js";
 import { generateJwt } from "../helpers/token.js";
-import { cookieOptions } from "../helpers/constants.js";
+import { passwordEmptyErrors } from "../helpers/constants.js";
+import { BadRequestError, UnauthenticatedError } from "./../helpers/errors.js";
+import { comparePass, hashPass } from "../helpers/password.js";
 
 export const registerUser = async (req, res) => {
-  const user = await User.create(req.body);
-  res.status(StatusCodes.CREATED).json({ user });
+  const hashedPassword = await hashPass(req.body.password);
+  req.body.password = hashedPassword;
+
+  await User.create(req.body);
+  res.status(StatusCodes.CREATED).json({ message: "user created" });
 };
 
 export const loginUser = async (req, res) => {
-  const user = await User.findOne({ email: req.body.email }).lean();
-  const access_token = generateJwt({ userId: user._id, role: user.role });
+  const { password, email } = req.body;
+  const user = await User.findOne({ email }).lean();
 
-  res.cookie("access_token", access_token, cookieOptions);
+  if (!user.password) {
+    throw new BadRequestError(passwordEmptyErrors);
+  }
+
+  const access_token = generateJwt({ userId: user?._id, role: user.role });
+
+  const isValidPassword = await comparePass(password, user.password);
+  if (!isValidPassword) throw new UnauthenticatedError("invalid credentials");
+
   res.status(StatusCodes.OK).json({ access_token });
 };
