c84a13b3d5fc129557d6ffacda401f2ae821f366
svkhrobbeck <svkhrobbeck@gmail.com>
2023-12-14 23:12:50 +0500

    complete todo route

--- server/src/routes/todo.js
+++ server/src/routes/todo.js
@@ -1,5 +1,24 @@
 import { Router } from "express";
+import {
+  createTodo,
+  deleteOneTodo,
+  editTodo,
+  editTodoStatus,
+  getAllTodo,
+  getTodoById,
+} from "../controllers/todo.js";
+import { valCreateTodo, valTodoIdParam } from "../middlewares/validation.js";
 
 const router = Router();
 
+router.route("/").get(getAllTodo).post(valCreateTodo, createTodo);
+router
+  .route("/:id")
+  .get(valTodoIdParam, getTodoById)
+  .patch(valTodoIdParam, editTodo)
+  .delete(valTodoIdParam, deleteOneTodo);
+router.patch("/status/:id", valTodoIdParam, editTodoStatus);
+
 export default router;
+
+// not authorized to access this route
