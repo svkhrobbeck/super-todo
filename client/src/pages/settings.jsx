af32a2f6b9afa21ab6125292afbdb8f9241c6e40
svkhrobbeck <svkhrobbeck@gmail.com>
2023-12-17 04:07:40 +0500

    create settings page completely

---
+++ client/src/pages/settings.jsx
@@ -0,0 +1,62 @@
+import { Helmet } from "react-helmet";
+import { useDashboardContext } from "../layouts/dashboard-layout";
+import { Form, Link, redirect } from "react-router-dom";
+import { FormInput, SubmitBtn } from "../components";
+import errorToast from "../helpers/errorToast";
+import axios from "axios";
+
+export const settingsAction = async ({ request }) => {
+  const formData = await request.formData();
+  const payload = Object.fromEntries(formData);
+
+  try {
+    await axios.patch("/user/profile", payload);
+    return redirect("..");
+  } catch (err) {
+    errorToast(err);
+    return err;
+  }
+};
+
+const SettingsPage = () => {
+  const { user, signOutUser } = useDashboardContext();
+
+  return (
+    <>
+      <Helmet>
+        <title>Todo Application | Settings</title>
+      </Helmet>
+      <section className="flex justify-center">
+        <Form className="p-[10px] max-w-[700px] w-full" method="PATCH">
+          <h2 className="font-bold text-[42px] text-center">Update Profile</h2>
+          <div className="pt-[10px] flex justify-center items-center gap-[12px]">
+            <img
+              className="w-[100px] rounded-full"
+              src={user.picture}
+              alt={user.name}
+            />
+            <div>
+              <p className="font-bold text-[26px] lowercase">{user.email}</p>
+              <p className="font-medium text-[18px] capitalize">{user.name}</p>
+            </div>
+          </div>
+          <FormInput name="email" defaultValue={user.email} required />
+          <FormInput name="name" defaultValue={user.name} required />
+          <div className="flex gap-[12px] justify-end">
+            <Link className="btn-teal" to="../update-password">
+              update/set password
+            </Link>
+            <Link className="btn-blue" to="..">
+              back to dashboard
+            </Link>
+            <SubmitBtn text="update profile" />
+            <button className="btn-red" type="button" onClick={signOutUser}>
+              sign out
+            </button>
+          </div>
+        </Form>
+      </section>
+    </>
+  );
+};
+export default SettingsPage;
