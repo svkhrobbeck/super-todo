b4183fe8f28510c90e13c5f3dfbbf20beb5bb567
svkhrobbeck <svkhrobbeck@gmail.com>
2023-12-17 04:03:40 +0500

    update user router

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,8 +1,23 @@
 import { Router } from "express";
-import { getUserProfile } from "../controllers/user.js";
+import {
+  getUserProfile,
+  updatePassword,
+  updateUserProfile,
+} from "../controllers/user.js";
+import {
+  valSetPassword,
+  valUpdatePassword,
+  valUserUpdate,
+} from "../middlewares/validation.js";
 
 const router = Router();
 
-router.get("/profile", getUserProfile);
+router
+  .route("/profile")
+  .get(getUserProfile)
+  .patch(valUserUpdate, updateUserProfile);
+
+router.patch("/profile/update-password", valUpdatePassword, updatePassword);
+router.patch("/profile/set-password", valSetPassword, updatePassword);
 
 export default router;
