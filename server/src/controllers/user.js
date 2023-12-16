d1b64414186b152dbb4ab9890432cf5872d4ce1a
svkhrobbeck <svkhrobbeck@gmail.com>
2023-12-17 04:03:00 +0500

    create update updateUserProfile controller

--- server/src/controllers/user.js
+++ server/src/controllers/user.js
@@ -8,3 +8,10 @@ export const getUserProfile = async (req, res) => {
 
   res.status(StatusCodes.OK).json({ user: filteredUser });
 };
+
+export const updateUserProfile = async (req, res) => {
+  const { email, name } = req.body;
+  await User.findByIdAndUpdate(req.user.userId, { email, name }, { new: true });
+
+  res.status(StatusCodes.OK).json({ message: "user updated" });
+};
