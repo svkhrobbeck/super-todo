8f5f006912571f839a8f7b69cda38b7a2db09af9
svkhrobbeck <svkhrobbeck@gmail.com>
2023-12-17 04:04:52 +0500

    create DashboardContext

--- client/src/layouts/dashboard-layout.jsx
+++ client/src/layouts/dashboard-layout.jsx
@@ -4,12 +4,16 @@ import axios from "axios";
 import { Header } from "./../components";
 import { toast } from "react-toastify";
 import storage from "../helpers/storage";
+import { createContext, useContext, useState } from "react";
+
+const DashboardContext = createContext();
 
 export const dashboardLoader = async () => {
   try {
     const { data } = await axios.get("/user/profile");
     return data;
   } catch (err) {
+    storage.remove("access_token");
     return redirect("/");
   }
 };
@@ -17,17 +21,26 @@ export const dashboardLoader = async () => {
 const DashboardLayout = () => {
   const { user } = useLoaderData();
   const navigate = useNavigate();
+  const [passwordType, setPasswordType] = useState("");
 
-  const logoutUser = () => {
+  const signOutUser = () => {
     storage.remove("access_token");
     navigate("/sign-in");
-    toast.success("Logging out...");
+    toast.success("signing out...");
   };
 
   return (
-    <>
-      <Outlet />
-    </>
+    <DashboardContext.Provider
+      value={{ user, signOutUser, passwordType, setPasswordType }}
+    >
+      <Header />
+      <main>
+        <Outlet context={{ user }} />
+      </main>
+    </DashboardContext.Provider>
   );
 };
+
+export const useDashboardContext = () => useContext(DashboardContext);
+
 export default DashboardLayout;
