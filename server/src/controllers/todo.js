a5b3f81251d3074585ba79baaa630a17f3690409
svkhrobbeck <svkhrobbeck@gmail.com>
2023-12-17 21:25:35 +0500

    update todo controllers

--- server/src/controllers/todo.js
+++ server/src/controllers/todo.js
@@ -2,39 +2,36 @@ import { StatusCodes } from "http-status-codes";
 import Todo from "../models/todo.js";
 
 export const getAllTodo = async (req, res) => {
-  // const page = +(req.query?.page || 1);
-  // const limit = +(req.query?.limit || null);
-
-  // const skip = (page - 1) * limit;
+  const { search } = req.query;
   const author = req.user.userId;
 
-  const all_todo = await Todo.find({ author })
-  // .skip(skip).limit(limit);
-  const total_todo = await Todo.countDocuments({ author });
+  const task = new RegExp(search, "ig");
 
+  const todoParams = { author, task };
+  const total_todo = await Todo.countDocuments(todoParams);
+
+  const all_todo = await Todo.find(todoParams);
   res.status(StatusCodes.OK).json({ all_todo, total_todo });
 };
 
 export const createTodo = async (req, res) => {
-  const body = { author: req.user.userId, task: req.body.task };
-  const todo = await Todo.create(body);
+  const { task, details } = req.body;
+  const body = { author: req.user.userId, task, details };
 
+  const todo = await Todo.create(body);
   res.status(StatusCodes.CREATED).json({ todo });
 };
 
 export const getTodoById = async (req, res) => {
-  const todo = await Todo.findById(req.params.id);
+  const todo = await Todo.findById(req.params.id).lean();
   res.status(StatusCodes.OK).json({ todo });
 };
 
 export const editTodo = async (req, res) => {
-  const { task, status } = req.body;
-  const todo = await Todo.findByIdAndUpdate(
-    req.params.id,
-    { task, status },
-    { new: true }
-  );
+  const { task, status, details } = req.body;
+  const body = { task, status, details };
 
+  const todo = await Todo.findByIdAndUpdate(req.params.id, body, { new: true });
   res.status(StatusCodes.OK).json({ todo });
 };
 
