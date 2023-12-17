88f235e9f9381bda36aece77f8f4859392d30a78
svkhrobbeck <svkhrobbeck@gmail.com>
2023-12-17 21:27:57 +0500

    update todo slice

--- client/src/slices/todo.js
+++ client/src/slices/todo.js
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = { all_todo: [], currentTodo: {} };
+const initialState = { all_todo: [], total_pages: 0, currentTodo: {} };
 
 const todoSlice = createSlice({
   name: "todo",
@@ -9,8 +9,11 @@ const todoSlice = createSlice({
     setTodo(state, action) {
       state.all_todo = action.payload;
     },
+    setTotalPages(state, action) {
+      state.total_pages = action.payload;
+    },
   },
 });
 
-export const { setTodo } = todoSlice.actions;
+export const { setTodo, setTotalPages } = todoSlice.actions;
 export default todoSlice.reducer;
