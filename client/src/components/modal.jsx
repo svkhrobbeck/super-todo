8f4a10bdb755e060b33dad5a809289a87b2e9ce1
svkhrobbeck <svkhrobbeck@gmail.com>
2023-12-17 21:23:32 +0500

    update modal component

--- client/src/components/modal.jsx
+++ client/src/components/modal.jsx
@@ -4,10 +4,10 @@ import { createPortal } from "react-dom";
 
 const Modal = ({ modalClose, children }) => {
   return createPortal(
-    <div className="flex items-center justify-center fixed inset-0 bg-black/[.6] z-[300] p-[10px]">
-      <div className="bg-white relative py-[20px] px-[10px] max-w-[700px] w-full rounded-md flex flex-col">
+    <div className="flex items-center justify-center fixed inset-0 bg-black/[.8] z-[300] p-[10px]">
+      <div className="bg-white relative pt-[20px] pb-[8px] px-[20px] max-w-[700px] w-full rounded-md flex flex-col">
         <button className="absolute top-[2px] right-[2px]" onClick={modalClose}>
-          <IoMdClose className="text-[24px]" />
+          <IoMdClose className="text-[24px] text-teal-600" />
         </button>
         {children}
       </div>
