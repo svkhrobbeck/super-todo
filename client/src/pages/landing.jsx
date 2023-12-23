3b43d4f6c435dbeccd2d37e11b5a2f5541c31b16
svkhrobbeck <svkhrobbeck@gmail.com>
2023-12-23 20:07:37 +0500

    update landing page

--- client/src/pages/landing.jsx
+++ client/src/pages/landing.jsx
@@ -23,35 +23,30 @@ const Landing = () => {
       </Helmet>
 
       <section className="w-full h-screen">
-        <div className="flex flex-col h-full justify-center">
-          <img
-            className="block cursor-pointer mx-auto mb-[80px] w-[250px] 2xl:w-[235px] xl:w-[210px] lg:w-[190px] md:w-[175px] sm:w-[170px]"
-            src="/images/logo-teal.svg"
-            alt="logo teal"
-          />
-          <div className="flex gap-[30px] lg:gap-[20px] items-center justify-center md:text-center">
-            <div className="flex flex-col">
-              <h2 className="text-[48px] xl:text-[45px] lg:text-[38px] md:text-[34px] mb-[12px] font-semibold">
-                Todo Application
-              </h2>
-              <p className="text-emerald-700 font-medium mb-[10px]">
-                (with google authentication)
-              </p>
-              <div className="flex gap-[10px] md:justify-center">
-                <Link className="btn-teal" to="/sign-up">
-                  Sign Up
-                </Link>
-                <Link className="btn-teal" to="/sign-in">
-                  Sign In
-                </Link>
-              </div>
-            </div>
+        <div className="h-full flex gap-[30px] lg:gap-[20px] items-center justify-center md:text-center">
+          <div className="flex flex-col">
             <img
-              className="md:hidden w-[450px] xl:w-[400px] lg:w-[355px] md:w-[320px] h-auto"
-              src={landingImg}
-              alt="landing illustration"
+              className="mb-[12px] block cursor-pointer w-[290px] 2xl:w-[275px] xl:w-[250px] lg:w-[230px] md:w-[215px] sm:w-[180px] xs:w-[165px]"
+              src="/images/logo-teal.svg"
+              alt="logo teal"
             />
+            <p className="text-teal-600 font-medium mb-[10px]">
+              (with google authentication)
+            </p>
+            <div className="flex gap-[10px] md:justify-center">
+              <Link className="btn-teal" to="/sign-up">
+                Sign Up
+              </Link>
+              <Link className="btn-teal" to="/sign-in">
+                Sign In
+              </Link>
+            </div>
           </div>
+          <img
+            className="md:hidden w-[450px] xl:w-[400px] lg:w-[355px] md:w-[320px] h-auto"
+            src={landingImg}
+            alt="landing illustration"
+          />
         </div>
       </section>
     </>
