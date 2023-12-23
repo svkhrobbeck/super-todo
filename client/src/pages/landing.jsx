b26368d394fb7e3e5fc6876a186fcfb85f901998
svkhrobbeck <svkhrobbeck@gmail.com>
2023-12-23 20:23:51 +0500

    make responsive landing page

--- client/src/pages/landing.jsx
+++ client/src/pages/landing.jsx
@@ -23,10 +23,10 @@ const Landing = () => {
       </Helmet>
 
       <section className="w-full h-screen">
-        <div className="h-full flex gap-[30px] lg:gap-[20px] items-center justify-center md:text-center">
-          <div className="flex flex-col">
+        <div className="h-full flex gap-[30px] lg:gap-[20px] items-center justify-center md:flex-col-reverse md:text-center">
+          <div className="flex flex-col md:items-center md:w-full">
             <img
-              className="mb-[12px] block cursor-pointer w-[290px] 2xl:w-[275px] xl:w-[250px] lg:w-[230px] md:w-[215px] sm:w-[180px] xs:w-[165px]"
+              className="mb-[12px] block cursor-pointer w-[290px] 2xl:w-[275px] xl:w-[250px] lg:w-[230px] md:w-[215px] sm:w-[80%]"
               src="/images/logo-teal.svg"
               alt="logo teal"
             />
@@ -43,7 +43,7 @@ const Landing = () => {
             </div>
           </div>
           <img
-            className="md:hidden w-[450px] xl:w-[400px] lg:w-[355px] md:w-[320px] h-auto"
+            className="w-[450px] xl:w-[400px] lg:w-[355px] md:w-[320px] md:mb-[24px] h-auto"
             src={landingImg}
             alt="landing illustration"
           />
