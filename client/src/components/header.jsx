7558938a1e06b0b45dcb7f2e8400d00a9aa9b432
svkhrobbeck <svkhrobbeck@gmail.com>
2023-12-23 19:44:42 +0500

    make header component and inner elements responsive

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -21,28 +21,34 @@ const Header = () => {
   const modalClose = () => setModal(false);
 
   return (
-    <header className="sticky flex justify-between items-center w-full py-[14px] px-[25px] bg-teal-600 text-teal-900">
+    <header className="sticky top-0 z-[400] flex justify-between items-center w-full py-[14px] px-[25px] bg-teal-600 text-teal-900">
       <Link className="flex items-center !outline-white" to=".">
-        <img src={logo} alt="super todo logo" />
+        <img
+          className="w-[150px] 2xl:w-[145px] xl:w-[135px] lg:w-[130px] md:w-[125px] sm:w-[120px]"
+          src={logo}
+          alt="super todo logo"
+        />
       </Link>
       <div className="flex items-center gap-[7px]">
-        <p className="font-medium select-none">{time}</p>
+        <p className="font-medium select-none lg:text-[16px] md:text-[15px] whitespace-nowrap">
+          {time}
+        </p>
         <button className="flex" onClick={() => setModal(true)}>
-          <BiAddToQueue className="text-[25px]" />
+          <BiAddToQueue className="text-[25px] xl:text-[23px]" />
         </button>
         <button className="rounded-full">
-          <FaMoon className="text-[22px]" />
-          {/* <FaSun className="text-[22px]" /> */}
+          <FaMoon className="text-[22px] xl:text-[20px]" />
+          {/* <FaSun className="text-[22px]  xl:text-[20px]" /> */}
         </button>
         <Link className="flex rounded-full" to="settings">
           {user?.picture ? (
             <img
-              className="w-[30px] rounded-full aspect-[1/1]"
+              className="w-[30px] xl:w-[28px] lg:w-[26px] rounded-full aspect-[1/1]"
               src={user.picture}
               alt="user avatar"
             />
           ) : (
-            <VscAccount className="text-[30px] rounded-full" />
+            <VscAccount className="text-[30px] xl:w-[28px] lg:w-[26px] rounded-full" />
           )}
         </Link>
       </div>
