5f653f01abfdfcc6bac44b99a5e1c33ade99e130
svkhrobbeck <svkhrobbeck@gmail.com>
2023-12-23 19:44:59 +0500

    make modal component responsive

--- client/src/components/modal.jsx
+++ client/src/components/modal.jsx
@@ -1,11 +1,10 @@
-import { useDashboardContext } from "../layouts/dashboard-layout";
 import { IoMdClose } from "react-icons/io";
 import { createPortal } from "react-dom";
 
 const Modal = ({ modalClose, children }) => {
   return createPortal(
     <div className="flex items-center justify-center fixed inset-0 bg-black/[.8] z-[300] p-[10px]">
-      <div className="bg-white relative pt-[20px] pb-[8px] px-[20px] max-w-[700px] w-full rounded-md flex flex-col">
+      <div className="bg-white relative pt-[20px] pb-[8px] px-[20px] lg:px-[15px] md:px-[12px] sm:px-[10px] max-w-[700px] w-full rounded-md flex flex-col">
         <button className="absolute top-[2px] right-[2px]" onClick={modalClose}>
           <IoMdClose className="text-[24px] text-teal-600" />
         </button>
